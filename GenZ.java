52a3dca64c9b89db7667c51841f4aae64a762b8a
Tommy Hank Graham <tommy.h.graham-1@ou.edu>
2018-10-10 14:39:05 -0500

    with extends and super

--- GenZ.java
+++ GenZ.java
@@ -11,7 +11,10 @@ public class GenZ
                   Comparable<Date> d =  new Date();
                   System.out.println(d.compareTo(new Date(2008,1,1)));
                 
+                  ArrayList<? extends Number> nlist = new ArrayList<>();
+                  ArrayList<? super Integer> xnlist = new ArrayList<>();
                   ArrayList<Integer> list = new ArrayList<>();
+                  nlist = list;
                   list.add(d);
                   list.add(new Integer(4));
                   list.add(new Integer(44));
@@ -19,7 +22,7 @@ public class GenZ
                   list.add(new String("a"));
                   Integer sum = 0;
                   
-                  for (Number o: list)
+                  for (Number o: nlist)
                   {
                       System.out.println(o);
                       sum += (Integer)o;
