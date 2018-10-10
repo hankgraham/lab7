eaf3eb090a50d2424303fd8184c1c96a5d41bd1f
Tommy Hank Graham <tommy.h.graham-1@ou.edu>
2018-10-10 14:33:59 -0500

    with sum

--- GenZ.java
+++ GenZ.java
@@ -11,14 +11,23 @@ public class GenZ
                   Comparable<Date> d =  new Date();
                   System.out.println(d.compareTo(new Date(2008,1,1)));
                 
-                  ArrayList list = new ArrayList();
+                  ArrayList<Integer> list = new ArrayList<>();
                   list.add(d);
                   list.add(new Integer(4));
-                  for (Object o: list)
+                  list.add(new Integer(44));
+                  list.add(new Integer(44));
+                  list.add(new String("a"));
+                  Integer sum = 0;
+                  
+                  for (Number o: list)
                   {
                       System.out.println(o);
+                      sum += (Integer)o;
+                      
                   }
+                  System.out.println(sum);
      }
+    
     
 
 }
