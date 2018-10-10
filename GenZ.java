56df78ad5cabfc026f41b77d4a84fd27c5a52c65
Tommy Hank Graham <tommy.h.graham-1@ou.edu>
2018-10-10 14:26:22 -0500

    with generics

--- GenZ.java
+++ GenZ.java
@@ -1,12 +1,24 @@
-
+import java.util.ArrayList;
+import java.util.Date;
 
 public class GenZ
 {
 
     public static void main(String[] args)
-    {
-        // TODO Auto-generated method stub
-
-    }
+    { 
+                
+                  System.out.println("one day i will learn");
+                  Comparable<Date> d =  new Date();
+                  System.out.println(d.compareTo(new Date(2008,1,1)));
+                
+                  ArrayList list = new ArrayList();
+                  list.add(d);
+                  list.add(new Integer(4));
+                  for (Object o: list)
+                  {
+                      System.out.println(o);
+                  }
+     }
+    
 
 }
